3efe047c3a04d2d244726234c7deaff42e789480
Jacob Greenwald <jeg4t@virginia.edu>
2019-01-27 17:34:04 -0500

    adding default state temporarily to see if logic is working correctly

--- src/reducers/pieCharts.js
+++ src/reducers/pieCharts.js
@@ -1,4 +1,7 @@
-const pieCharts = (state = [], action) => {
+const pieCharts = (
+  state = [{ id: 0, number: 10, total: 20 }, { id: 1, number: 30, total: 40 }],
+  action
+) => {
   switch (action.type) {
     case 'ADD_PIE_CHART':
       return [
