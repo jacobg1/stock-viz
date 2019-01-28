eaa5cd82bbb45cf3c7322c9b7428a87d50ac790f
Jacob Greenwald <jeg4t@virginia.edu>
2019-01-27 19:20:12 -0500

    adding id to div

--- src/components/PieChartList.js
+++ src/components/PieChartList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PieChart from './PieChart'
 
 const PieChartList = ({ pieCharts }) => (
-  <div>
+  <div id="pieCharts">
     {pieCharts.map(pieChart => (
       <PieChart key={pieChart.id} {...pieChart} />
     ))}
