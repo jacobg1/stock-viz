dde1d637d6cd71c05ad1e7d2517538f7ab88fc12
Jacob Greenwald <jeg4t@virginia.edu>
2019-01-28 20:36:51 -0500

    adding functions to position lables around pie chart

--- src/components/PieSlice.js
+++ src/components/PieSlice.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import * as d3 from 'd3'
 
+function positionLabels(center) {
+  const x = center[0],
+    y = center[1],
+    h = Math.sqrt(x * x + y * y),
+    translateX = (x / h) * 130,
+    translateY = (y / h) * 130
+
+  return `translate(${translateX}, ${translateY})`
+}
+function startOrEnd(item) {
+  const startAngle = item.startAngle,
+    endAngle = item.endAngle,
+    compareAngles = (endAngle + startAngle) / 2
+	
+  return compareAngles > Math.PI ? 'end' : 'start'
+}
 const PieSlice = ({ slice }) => {
   const arc = d3
     .arc()
@@ -19,7 +35,12 @@ const PieSlice = ({ slice }) => {
         <text transform={`translate(${center})`} dy=".35em" className="label">
           {item.value}
         </text>
-        <text transform={`translate(${center})`} className="tooltip">
+        <text
+          dy=".35em"
+          fill="#ffffff"
+          transform={positionLabels(center)}
+          textAnchor={startOrEnd(item)}
+        >
           {item.value}
         </text>
       </g>
