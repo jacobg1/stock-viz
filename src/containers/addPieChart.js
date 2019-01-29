759190688f43fd93cf1e0fdf4fa76f780e4b69ec
Jacob Greenwald <jeg4t@virginia.edu>
2019-01-28 21:58:39 -0500

    adding input for title

--- src/containers/addPieChart.js
+++ src/containers/addPieChart.js
@@ -4,18 +4,25 @@ import { addPieChart } from '../actions'
 
 const AddPieChart = ({ dispatch }) => {
   let data
+  let title
 
   return (
     <div>
       <form
         onSubmit={e => {
           e.preventDefault()
-          dispatch(addPieChart(data.value))
+          dispatch(addPieChart(data.value, title.value))
           data.value = ''
+          title.value = ''
         }}
       >
         <input
           ref={node => (data = node)}
+          placeholder="input data separate numbers by ,"
+          type="text"
+        />
+        <input
+          ref={node => (title = node)}
           placeholder="input data separate numbers by ,"
           type="text"
         />
