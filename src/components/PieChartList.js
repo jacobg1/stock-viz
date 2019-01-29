6191c1c21f3e321ff25bfb375a402bc512ff4cc3
Jacob Greenwald <jeg4t@virginia.edu>
2019-01-28 21:58:13 -0500

    refactoring

--- src/components/PieChartList.js
+++ src/components/PieChartList.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import PieChart from './PieChart'
 
+/*
+  pieCharts: an array of objects, each object represents a pie chart
+  Loops through state and displays each individual pie chart
+*/
 const PieChartList = ({ pieCharts }) => (
   <div id="pieCharts">
     {pieCharts.map(pieChart => (
