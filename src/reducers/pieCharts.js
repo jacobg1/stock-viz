52eec2e31723053d082c933bc2bcbc841f268ca8
Jacob Greenwald <jeg4t@virginia.edu>
2019-01-29 20:55:49 -0500

    adding toggle percent reducer

--- src/reducers/pieCharts.js
+++ src/reducers/pieCharts.js
@@ -6,9 +6,16 @@ const pieCharts = (state = [], action) => {
         {
           id: action.id,
           data: action.data,
-          title: action.title
+          title: action.title,
+          isPercent: false
         }
       ]
+    case 'TOGGLE_PERCENT':
+      return state.map(pieChart =>
+        pieChart.id === action.id
+          ? { ...pieChart, isPercent: !pieChart.isPercent }
+          : pieChart
+      )
     default:
       return state
   }
