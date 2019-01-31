c28cb68fab34be468b7c9e854459ff41677d9081
Jacob Greenwald <jeg4t@virginia.edu>
2019-01-30 19:44:25 -0500

    remove console logs

--- src/components/InnerLabel.js
+++ src/components/InnerLabel.js
@@ -1,14 +1,13 @@
 import React from 'react'
+
 /*
   children: the label to display (props)
   center: center of the chart calculated using d3 (props)
 */
 function calculatePercent(children, total) {
-  //   console.log(total)
   return `${((children / total) * 100).toFixed(2)}%`
 }
 const InnerLabel = ({ children, center, total, isPercent }) => {
-  //   console.log(typeof children)
   return (
     <text transform={`translate(${center})`} dy=".35em" className="label">
       {isPercent ? calculatePercent(children, total) : children}
