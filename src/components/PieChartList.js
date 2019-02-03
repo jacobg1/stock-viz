9eaf606245896101a9a9acae26c8382f8fee14cd
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-02 21:11:11 -0500

    update to purecomponent

--- src/components/PieChartList.js
+++ src/components/PieChartList.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PieChart from './PieChart'
 
 /*
   pieCharts: an array of objects, each object represents a pie chart
   Loops through state and displays each individual pie chart
 */
-const PieChartList = ({ pieCharts, togglePercent }) => (
-  <div id="pieCharts">
-    {pieCharts.map(pieChart => (
-      <PieChart
-        key={pieChart.id}
-        {...pieChart}
-        onClick={() => togglePercent(pieChart.id)}
-      />
-    ))}
-  </div>
-)
+class PieChartList extends PureComponent {
+  render() {
+    console.log('test')
+    const { pieCharts, togglePercent } = this.props
+    return (
+      <div id="pieCharts">
+        {pieCharts.map(pieChart => (
+          <PieChart
+            key={pieChart.id}
+            {...pieChart}
+            onClick={() => togglePercent(pieChart.id)}
+          />
+        ))}
+      </div>
+    )
+  }
+}
 
 export default PieChartList
