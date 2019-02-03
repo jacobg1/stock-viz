9c7e65782c706c78f6fab591fc85bbd37b1f1bd3
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-02 22:39:50 -0500

    adding input for pie chart labels

--- src/containers/addPieChart.js
+++ src/containers/addPieChart.js
@@ -5,20 +5,27 @@ import { addPieChart } from '../actions'
 const AddPieChart = ({ dispatch }) => {
   let data
   let title
+  let labels
 
   return (
     <div>
       <form
         onSubmit={e => {
           e.preventDefault()
-          dispatch(addPieChart(data.value, title.value))
+          dispatch(addPieChart(data.value, title.value, labels.value))
           data.value = ''
           title.value = ''
+          labels.value = ''
         }}
       >
         <input
           ref={node => (data = node)}
           placeholder="input data separate numbers by ,"
+          type="text"
+        />
+        <input
+          ref={node => (labels = node)}
+          placeholder="input labels separate numbers by ,"
           type="text"
         />
         <input ref={node => (title = node)} placeholder="title" type="text" />
