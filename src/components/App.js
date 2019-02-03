7d8ff70c95bb0783c26c80172c5ffac486860c7f
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-02 22:46:02 -0500

    remove uneeded import

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import AddPieChart from '../containers/AddPieChart'
 import VisiblePieChartList from '../containers/VisiblePieChartList'
