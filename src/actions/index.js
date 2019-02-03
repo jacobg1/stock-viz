d0a9d51c9340bd8672541668d940f4367e1b8143
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-02 22:39:15 -0500

    adding labels to action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 let nextPieChartId = 0
-export const addPieChart = (data, title, isPercent) => ({
+export const addPieChart = (data, title, labels, isPercent) => ({
   type: 'ADD_PIE_CHART',
   id: nextPieChartId++,
   data,
   title,
+  labels,
   isPercent
 })
 
