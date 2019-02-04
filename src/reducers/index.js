b1e22d70c38c131b3629633b63ab21d9ee266e2e
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-03 20:48:46 -0500

    add in add line graph reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import pieCharts from './pieCharts'
 import chartFilters from './chartFilters'
+import lineGraphs from './lineGraphs'
 
 export default combineReducers({
   pieCharts,
-  chartFilters
+  chartFilters,
+  lineGraphs
 })
