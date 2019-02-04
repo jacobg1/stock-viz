54bf29a06a744b4969e888f36bc218f5bca7fc06
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-03 20:24:13 -0500

    conditionally show charts based on filter selection

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import './App.css'
 import AddPieChart from '../containers/AddPieChart'
 import VisiblePieChartList from '../containers/VisiblePieChartList'
+import FilterCharts from '../containers/FilterCharts'
+import { ChartFilters } from '../actions'
 
 class Charts extends Component {
   render() {
+    const { chartsToShow } = this.props
+    console.log(chartsToShow)
     return (
       <div className="App">
+        <FilterCharts filter={ChartFilters.SHOW_PIE_CHARTS}>
+          Pie Charts
+        </FilterCharts>
+        <FilterCharts filter={ChartFilters.SHOW_LINE_GRAPHS}>
+          Line Graphs
+        </FilterCharts>
         <header className="App-header">
-          <AddPieChart />
-          <VisiblePieChartList />
+          {chartsToShow === 'SHOW_PIE_CHARTS' && (
+            <>
+              <AddPieChart />
+              <VisiblePieChartList />
+            </>
+          )}
+          {chartsToShow === 'SHOW_LINE_GRAPHS' && (
+            <>
+              <p>line graphs!!!</p>
+            </>
+          )}
         </header>
       </div>
     )
   }
 }
 
-export default Charts
+const mapStateToProps = state => ({
+  chartsToShow: state.chartFilters
+})
+
+const ChartsContainer = connect(mapStateToProps)(Charts)
+
+export default ChartsContainer
