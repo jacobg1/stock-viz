e209e39d241403ce5ba57f184b8f802e8cd4ce96
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-16 16:15:54 -0500

    adding button to dispatch action to fetch stock prices

--- src/components/Stocks.js
+++ src/components/Stocks.js
@@ -3,11 +3,19 @@ import { connect } from 'react-redux'
 import { getPrices } from '../actions/stockActions'
 
 class Stocks extends Component {
-  componentDidMount() {
-    this.props.dispatch(getPrices())
-  }
+  // componentDidMount() {
+  //   this.props.dispatch(getPrices())
+  // }
   render() {
-    return <p>test</p>
+    return (
+      <button
+        onClick={() => {
+          this.props.dispatch(getPrices())
+        }}
+      >
+        fetch prices
+      </button>
+    )
   }
 }
 
