92ae53056beb8ee87280b2e69b120f9608bf75a0
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-16 17:53:43 -0500

    adding stocks container and mapping state to props

---
+++ src/containers/stocks/StocksContainer.js
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux'
+import Stocks from '../../components/Stocks'
+
+const mapStateToProps = state => ({
+	prices: state.stocks.prices,
+	loading: state.stocks.loading,
+	error: state.stocks.error,
+	meta: state.stocks.meta
+})
+
+export default connect(mapStateToProps)(Stocks)
