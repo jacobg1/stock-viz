e23a00938b44a9bd4c39529a79f6ac73a6578578
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-16 17:54:32 -0500

    formatting

--- src/containers/stocks/StocksContainer.js
+++ src/containers/stocks/StocksContainer.js
@@ -2,10 +2,10 @@ import { connect } from 'react-redux'
 import Stocks from '../../components/Stocks'
 
 const mapStateToProps = state => ({
-	prices: state.stocks.prices,
-	loading: state.stocks.loading,
-	error: state.stocks.error,
-	meta: state.stocks.meta
+  prices: state.stocks.prices,
+  loading: state.stocks.loading,
+  error: state.stocks.error,
+  meta: state.stocks.meta
 })
 
 export default connect(mapStateToProps)(Stocks)
