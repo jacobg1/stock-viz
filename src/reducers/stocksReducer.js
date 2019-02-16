77cb988a616f8426a1904599dfdb288f93951956
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-16 14:33:02 -0500

    setting up reducer to handle fetching of stocks

---
+++ src/reducers/stocksReducer.js
@@ -0,0 +1,38 @@
+import {
+  GET_PRICES_BEGIN,
+  GET_PRICES_SUCCESS,
+  GET_PRICES_FAILURE
+} from '../actions/stockActions'
+
+const initialState = {
+  prices: [],
+  loading: false,
+  error: null
+}
+
+function stocksReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_PRICES_BEGIN:
+      return {
+        ...state,
+        loading: true,
+        error: null
+      }
+    case GET_PRICES_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        prices: action.payload.prices
+      }
+    case GET_PRICES_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        prices: []
+      }
+    default:
+      return state
+  }
+}
+export default stocksReducer
