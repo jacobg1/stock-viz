e5cd4bc664dd1400b9d73dd540c23b9184729c29
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-16 14:32:43 -0500

    adding stocks reducer to combine reducer function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import pieCharts from './pieCharts'
 import chartFilters from './chartFilters'
 import lineGraphs from './lineGraphs'
+import stocksReducer from './stocksReducer'
 
 export default combineReducers({
   pieCharts,
   chartFilters,
-  lineGraphs
+  lineGraphs,
+  stocksReducer
 })
