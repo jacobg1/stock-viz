6606a723d26915a47bdfa85f7138db4091e7f8c0
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-18 14:39:11 -0500

    adding hover dots

--- src/components/stockGraph/PriceDataLine.js
+++ src/components/stockGraph/PriceDataLine.js
@@ -1,9 +1,36 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import * as d3 from 'd3'
 
-class PriceDataLine extends Component {
+class PriceDataLine extends PureComponent {
+  constructor() {
+    super()
+    this.state = {
+      transform: '',
+      value: ''
+    }
+  }
+  hover(e) {
+    // <text transform={`translate(${x(price.x)}, ${y(price.y)})`}>{price.y}</text>
+    this.setState(
+      {
+        transform: `translate(${e.target.getAttribute(
+          'cx'
+        )},${e.target.getAttribute('cy')})`,
+        value: e.target.getAttribute('data-value')
+      },
+      () => {
+        console.log(this.state.value)
+      }
+    )
+  }
+  clearHover() {
+    this.setState({
+      transform: '',
+      value: ''
+    })
+  }
   render() {
-    const { priceLine, margin, h, w, stroke } = this.props
+    const { priceLine, margin, h, w, stroke, hover } = this.props
 
     // scale x axis to fit data
     const x = d3
@@ -25,8 +52,28 @@ class PriceDataLine extends Component {
       .y(d => y(d.y))
       .curve(d3.curveCatmullRom.alpha(0.5))
 
+    let hoverDots = priceLine.map((price, i) =>
+      // console.log(price.y)
+
+      hover ? (
+        <circle
+          onMouseEnter={e => this.hover(e)}
+          key={i}
+          // onMouseLeave={() => this.clearHover()}
+          fill={stroke}
+          cx={x(price.x)}
+          cy={y(price.y)}
+          r={2}
+          data-value={price.y}
+        />
+      ) : null
+    )
     return (
       <>
+        {hoverDots}
+        <g>
+          <text transform={`translate(375, 30)`}>{this.state.value}</text>
+        </g>
         <path className="path" stroke={stroke} d={line(priceLine)} />
       </>
     )
