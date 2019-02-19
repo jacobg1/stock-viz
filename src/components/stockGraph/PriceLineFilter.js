a147d32c51a5856841e96c01ac675fac9af6fb32
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-18 22:37:26 -0500

    changing div to fragment

--- src/components/stockGraph/PriceLineFilter.js
+++ src/components/stockGraph/PriceLineFilter.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
 // buttons for filtering between charts
 const PriceLineFilter = ({ children, onChange, checked }) => {
   console.log(checked)
   return (
-    <div>
+    <>
       <label>{children}</label>
       <input onChange={onChange} checked={checked} type="checkbox" />
-    </div>
+    </>
   )
 }
 
