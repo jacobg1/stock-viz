29d43afe2e53fc321c049d617992b3a9639b19fb
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-18 22:35:43 -0500

    adding component to conditionally show parts of legend

---
+++ src/components/stockGraph/Legend.js
@@ -0,0 +1,38 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+// buttons for filtering between charts
+const Legend = ({ stocklines }) => {
+  const { type } = stocklines
+  console.log(type)
+  return (
+    <div className="legend">
+      {type.high && (
+        <>
+          <div className="high" />
+          <span> - high</span>
+        </>
+      )}
+      {type.low && (
+        <>
+          <div className="low" />
+          <span> - low</span>
+        </>
+      )}
+      {type.open && (
+        <>
+          <div className="open" />
+          <span> - open</span>
+        </>
+      )}
+      {type.close && (
+        <>
+          <div className="close" />
+          <span> - close</span>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default Legend
