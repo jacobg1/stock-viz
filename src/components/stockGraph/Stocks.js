f87f13f98137edb9c85df747c71e693e49341412
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-18 22:37:05 -0500

    adding in legend component and wrapping filters in a div

--- src/components/stockGraph/Stocks.js
+++ src/components/stockGraph/Stocks.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PriceGraph from './PriceGraph'
 import LineFilter from '../../containers/stocks/LineFilter'
 import { StockLines } from '../../actions/stockActions'
-import { connect } from 'react-redux'
+import Legend from './Legend'
 
 class Stocks extends Component {
   // componentDidMount() {
@@ -24,25 +24,19 @@ class Stocks extends Component {
             <h2>Stock: {meta['2. Symbol']}</h2>
             {/* <p>{meta['1. Information']}</p> */}
             <p>Last updated: {meta['3. Last Refreshed']}</p>
-            <div className="legend">
-              <div className="high" />
-              <span> - high</span>
-              <div className="low" />
-              <span> - low</span>
-              <div className="open" />
-              <span> - open</span>
-              <div className="close" />
-              <span> - close</span>
-            </div>
+            <Legend stocklines={stockLines} />
           </div>
         )}
 
         {prices.length !== 0 && (
           <>
-            <LineFilter filter={'high'}>high</LineFilter>
-            <LineFilter filter={'low'}>low</LineFilter>
-            <LineFilter filter={'open'}>open</LineFilter>
-            <LineFilter filter={'close'}>close</LineFilter>
+            <div className="line-filters">
+              <LineFilter filter={StockLines.HIGH}>high</LineFilter>
+              <LineFilter filter={StockLines.LOW}>low</LineFilter>
+              <LineFilter filter={StockLines.OPEN}>open</LineFilter>
+              <LineFilter filter={StockLines.CLOSE}>close</LineFilter>
+            </div>
+
             <PriceGraph stockLines={stockLines} prices={prices} />
           </>
         )}
