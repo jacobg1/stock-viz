e00289c94e0121e3fb10945411ee2df4e9806155
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-18 20:56:53 -0500

    updating mapStateToProps function

--- src/containers/stocks/StocksContainer.js
+++ src/containers/stocks/StocksContainer.js
@@ -5,7 +5,8 @@ const mapStateToProps = state => ({
   prices: state.stocks.prices,
   loading: state.stocks.loading,
   error: state.stocks.error,
-  meta: state.stocks.meta
+  meta: state.stocks.meta,
+  stockLines: state.stockLines
 })
 
 export default connect(mapStateToProps)(Stocks)
