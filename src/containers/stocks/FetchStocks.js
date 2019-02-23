22949a6f886a1eb8ce753ae36b0f9db6b5d978f1
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-22 21:03:36 -0500

    add select choices to toggle search from month to day

--- src/containers/stocks/FetchStocks.js
+++ src/containers/stocks/FetchStocks.js
@@ -4,16 +4,24 @@ import { getPrices } from '../../actions/stockActions'
 
 const FetchStocks = ({ dispatch }) => {
   let symbol
-
+  let type = 'TIME_SERIES_MONTHLY'
   return (
     <div>
       <form
         onSubmit={e => {
           e.preventDefault()
-          dispatch(getPrices(symbol.value))
+          dispatch(getPrices(symbol.value, type))
           symbol.value = ''
         }}
       >
+        <select
+          onChange={e => {
+            type = e.target.value
+          }}
+        >
+          <option value="TIME_SERIES_MONTHLY">Monthly</option>
+          <option value="TIME_SERIES_DAILY">Daily</option>
+        </select>
         <input ref={node => (symbol = node)} placeholder="search" type="text" />
         <button type="submit">fetch prices</button>
       </form>
