4088452afa3f4fbe90a1fef328c933f25fab45a4
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-24 01:15:48 -0500

    setting default state to show crypto prices

--- src/reducers/chartFilters.js
+++ src/reducers/chartFilters.js
@@ -1,6 +1,6 @@
 import { ChartFilters } from '../actions'
 
-const chartFilters = (state = ChartFilters.SHOW_STOCK_PRICES, action) => {
+const chartFilters = (state = ChartFilters.SHOW_CRYPTO_PRICES, action) => {
   switch (action.type) {
     case 'SET_CHART_FILTER':
       return action.filter
