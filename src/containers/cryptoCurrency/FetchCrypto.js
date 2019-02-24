3aa06c32aeba863aa3e658724b20964f51658117
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-24 01:14:39 -0500

    adding select options for coin type and market

--- src/containers/cryptoCurrency/FetchCrypto.js
+++ src/containers/cryptoCurrency/FetchCrypto.js
@@ -1,28 +1,52 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { getCrypto } from '../../actions/cryptoActions'
+import coinTypes from '../../data/coinTypes.json'
+import currencies from '../../data/currencies.json'
 
 const FetchCrypto = ({ dispatch }) => {
-  let symbol
+  let symbol = 'BTC'
   let type = 'DIGITAL_CURRENCY_MONTHLY'
+  let currency = 'USD'
+
+  let listOfCoins = coinTypes.map((coin, index) => {
+    return (
+      <option value={coin.code} key={index}>
+        {coin.name}
+      </option>
+    )
+  })
+  let listOfCurrencies = currencies.map((currency, index) => {
+    return (
+      <option value={currency.currencyCode} key={index}>
+        {currency.currencyName}
+      </option>
+    )
+  })
+
   return (
     <div>
       <form
         onSubmit={e => {
           e.preventDefault()
-          dispatch(getCrypto(symbol.value, type, 'USD'))
-          symbol.value = ''
+          dispatch(getCrypto(symbol, type, currency))
         }}
       >
-        {/* <select
-					onChange={e => {
-						type = e.target.value
-					}}
-				>
-					<option value="DIGITAL_CURRENCY_MONTHLY">Monthly</option>
-					<option value="DIGITAL_CURRENCY_WEEKLY">Daily</option>
-				</select> */}
-        <input ref={node => (symbol = node)} placeholder="search" type="text" />
+        <select
+          onChange={e => {
+            console.log(e.target.value)
+            symbol = e.target.value
+          }}
+        >
+          {listOfCoins}
+        </select>
+        <select
+          onChange={e => {
+            currency = e.target.value
+          }}
+        >
+          {listOfCurrencies}
+        </select>
         <button type="submit">fetch prices</button>
       </form>
     </div>
