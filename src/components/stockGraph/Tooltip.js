e8ad72fac37d191e39105b7967e8e897c5245ad0
Jacob Greenwald <jeg4t@virginia.edu>
2019-02-24 20:30:25 -0500

    fixing issue with too many decimal point on hover

--- src/components/stockGraph/Tooltip.js
+++ src/components/stockGraph/Tooltip.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-/*
-  children: Number the label to display (props)
-  center: [x,y] center of the chart calculated using d3 (props)
-  total: Number sum of label values (props)
-  isPercent: boolean whether label has been changed to percent, default is false
-*/
+
 const formatDate = d => {
   let date = new Date(d)
   // console.log(date.getFullYear())
   return date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear()
 }
+const formatValue = n => {
+  return parseInt(n)
+}
 const Tooltip = ({ value, date, positionX, positionY }) => {
   return (
     <div className="hover-holder" style={{ top: positionY, left: positionX }}>
-      <span className="hover-text">Amount: {value}</span>
+      <span className="hover-text">Amount: {formatValue(value)}</span>
       <span className="hover-text">Date: {formatDate(date)}</span>
     </div>
   )
