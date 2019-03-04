67d900e9688601dbdb5114b3893cac2d882e51b7
Jacob Greenwald <jeg4t@virginia.edu>
2019-03-03 20:32:14 -0500

    creating component for list of crypto currencies

---
+++ src/components/cryptoGraphs/CryptoList.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { setCryptoCoin } from '../../actions/cryptoActions'
+import coinTypes from '../../data/coinTypes.json'
+import Select from 'react-select'
+import { customStyles } from '../../data/customStyles'
+
+const CryptoList = ({ cryotoCoin, onChange }) => {
+  
+  return (
+    <div className="select-holder">
+      <Select
+        classNamePrefix="select-input"
+				defaultValue={cryotoCoin.value}
+        onChange={(opt) => onChange(opt)}
+				options={coinTypes}
+        isSearchable
+        styles={customStyles}
+        placeholder="Type to start searching..."
+      />
+      {/* {
+				Object.keys(stockSymbol).length !== 0 && (
+					<>
+					<span>Stock: {stockSymbol.label}</span>
+						<span>Symbol: {stockSymbol.value}</span>
+					</>
+				)
+			} */}
+    </div>
+  )
+}
+const mapStateToProps = state => ({
+  cryotoCoin: state.cryptoCoin
+})
+const mapDispatchToProps = dispatch => ({
+	onChange: opt => dispatch(setCryptoCoin(opt.value, opt.label))
+})
+
+export default connect(
+	mapStateToProps, 
+	mapDispatchToProps
+)(CryptoList)
