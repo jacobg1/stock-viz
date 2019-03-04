8512cb7758e62e575e82b3c7cb12726846c3ab1b
Jacob Greenwald <jeg4t@virginia.edu>
2019-03-03 23:23:35 -0500

    changing default state to show nasdaq stocks first

--- src/reducers/listFilters.js
+++ src/reducers/listFilters.js
@@ -1,6 +1,6 @@
 import { ListFilters } from '../actions/stockActions'
 
-const listFilters = (state = ListFilters.SHOW_NYSE, action) => {
+const listFilters = (state = ListFilters.SHOW_NASDAQ, action) => {
   switch (action.type) {
     case 'SET_LIST_FILTER':
       return action.filter
