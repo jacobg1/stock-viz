0be7f368210676e1c41995d2c1951a8fc4239787
Jacob Greenwald <jeg4t@virginia.edu>
2019-03-03 20:35:20 -0500

    removing uneeded style object and pulling in state

--- src/components/stockGraph/SymbolList.js
+++ src/components/stockGraph/SymbolList.js
@@ -3,57 +3,16 @@ import { connect } from 'react-redux'
 import { setStockSymbol } from '../../actions/stockActions'
 import listOfStockSymbols from '../../data/stockSymbols.json'
 import Select from 'react-select'
+import { customStyles } from '../../data/customStyles'
 
-const SymbolList = ({ dispatch, stockSymbol }) => {
-  const customStyles = {
-    control: (base, state) => ({
-      ...base,
-      background: '#282c34',
-      borderRadius: state.isFocused ? '3px 3px 0 0' : 3,
-      boxSizing: 'border-box',
-      borderColor: state.isFocused ? '#95eaf1' : 'white',
-      boxShadow: state.isFocused ? null : null,
-      '&:hover': {
-        borderColor: state.isFocused ? 'rgb(239, 110, 141)' : '#a8a8ff'
-      }
-    }),
-    menu: base => ({
-      ...base,
-      borderRadius: 0,
-      hyphens: 'auto',
-      marginTop: 0,
-      textAlign: 'center',
-      wordWrap: 'break-word'
-    }),
-    menuList: base => ({
-      ...base,
-      padding: 0,
-      background: '#282c34'
-    }),
-    option: (provided, state) => ({
-      ...provided,
-      borderBottom: '1px dotted #95eaf1',
-      color: state.isSelected ? '#023950' : 'rgb(239, 110, 141)',
-      background: state.isSelected ? '#95eaf1' : '#282c34',
-      padding: 15,
-      '&:hover': {
-        color: 'white',
-        background: '#a8a8ff'
-      },
-      '&:focus': {
-        color: 'white',
-        background: '#a8a8ff'
-      },
-      cursor: 'pointer'
-    })
-  }
+const SymbolList = ({ stockSymbol, onChange }) => {
 
   return (
     <div className="select-holder">
       <Select
         classNamePrefix="select-input"
         defaultValue={stockSymbol.value}
-        onChange={opt => dispatch(setStockSymbol(opt.value, opt.label))}
+        onChange={(opt) => onChange(opt)}
         options={listOfStockSymbols}
         isSearchable
         styles={customStyles}
@@ -73,5 +32,11 @@ const SymbolList = ({ dispatch, stockSymbol }) => {
 const mapStateToProps = state => ({
   stockSymbol: state.stockSymbol
 })
+const mapDispatchToProps = dispatch => ({
+	onChange: opt => dispatch(setStockSymbol(opt.value, opt.label))
+})
 
-export default connect(mapStateToProps)(SymbolList)
+export default connect(
+	mapStateToProps, 
+	mapDispatchToProps
+)(SymbolList)
