e5936f8195178e1b160cd3ef8d78e9978feb3f85
Jacob Greenwald <jeg4t@virginia.edu>
2019-03-09 15:05:05 -0500

    adding breakpoins object

---
+++ src/styles/breakpoints.js
@@ -0,0 +1,9 @@
+export const breakpoints = {
+  header: `(min-width: 325px)`,
+  mobile: `(min-width: 575px)`,
+  footer: `(min-width: 560px)`,
+  specialHeader: `(min-width: 700px)`,
+  tablet: `(min-width: 768px)`,
+  laptop: `(min-width: 1024px)`,
+  desktop: `(min-width: 1440px)`
+}
