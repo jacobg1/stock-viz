2ead41e91301fb3682bc4276934a1a9729cc7715
Jacob Greenwald <jeg4t@virginia.edu>
2019-03-09 15:05:49 -0500

    formatting

--- src/reducers/cryptoType.js
+++ src/reducers/cryptoType.js
@@ -3,7 +3,7 @@ import { TypeFilters } from '../actions/cryptoActions'
 const cryptoType = (state = TypeFilters.MONTHLY, action) => {
   switch (action.type) {
     case 'SET_CRYPTO_TYPE':
-			return action.timePeriod
+      return action.timePeriod
     default:
       return state
   }
