4097bc2135a3002c70fbb409a175b00c45e302fb
Jacob Greenwald <jeg4t@virginia.edu>
2019-03-09 18:35:06 -0500

    custom radio buttons

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,12 +1,97 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import PropTypes from 'prop-types'
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+import { breakpoints } from '../styles/breakpoints'
+
+const txtColor = '#ffffff'
+const beforeColor = '#ffffff'
+const checkedBorderColor = '#a8a8ff'
+
+const filterBoxes = css`
+  display: block;
+  text-align: left;
+  &:first-of-type {
+    padding-bottom: 10px;
+  }
+  label {
+    cursor: pointer;
+    position: relative;
+    vertical-align: sub;
+    padding-top: 6px;
+    display: inline-block;
+    font-size: 17px;
+    color: ${txtColor};
+    @media ${breakpoints.mobile} {
+      font-size: 19px;
+    }
+  }
+  input {
+    display: none;
+    visibility: hidden;
+    &:checked ~ .custom-radio:before {
+      background-color: ${beforeColor};
+    }
+    &:checked ~ .custom-radio {
+      border-color: ${checkedBorderColor};
+    }
+    &:checked ~ span {
+      color: ${checkedBorderColor};
+    }
+  }
+  .custom-radio {
+    border: 5px solid ${txtColor};
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    float: left;
+    position: relative;
+    transition: border 0.3s linear;
+    @media ${breakpoints.mobile} {
+      width: 23px;
+      height: 23px;
+    }
+    &:after {
+      content: '';
+      display: table;
+      clear: both;
+    }
+    &:before {
+      display: block;
+      position: absolute;
+      content: '';
+      border-radius: 50%;
+      height: 12px;
+      width: 12px;
+      top: 4px;
+      left: 4px;
+      transition: background 0.25s linear;
+      @media ${breakpoints.mobile} {
+        height: 13px;
+        width: 13px;
+        top: 5px;
+        left: 5px;
+      }
+    }
+  }
+  span {
+    padding-left: 10px;
+    display: inline-block;
+    vertical-align: middle;
+    padding-top: 6px;
+  }
+`
 
 // buttons for filtering between charts
 const Filter = ({ children, onChange, active }) => {
   return (
-    <div>
-      <label>{children}</label>
-      <input onChange={onChange} checked={active} type="radio" />
+    <div css={filterBoxes}>
+      <label>
+        <input onChange={onChange} checked={active} type="radio" />
+        <span>{children}</span>
+        <div className="custom-radio" />
+      </label>
     </div>
   )
 }
