8e75f1a1ef8e5e096a4093c601b559a397aef692
Jacob Greenwald <jeg4t@virginia.edu>
2019-03-14 19:16:15 -0400

    applying same styles to crypto filters

--- src/components/cryptoGraphs/CryptoLineFilter.js
+++ src/components/cryptoGraphs/CryptoLineFilter.js
@@ -1,15 +1,92 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+import { breakpoints } from '../../styles/breakpoints'
 
+const txtColor = '#ffffff'
+const beforeColor = '#ffffff'
+const checkedBorderColor = '#a8a8ff'
+
+const filterBoxes = css`
+  display: inline-block;
+  text-align: left;
+  margin-right: 15px;
+  &:first-of-type {
+    padding-bottom: 10px;
+  }
+  label {
+    cursor: pointer;
+    position: relative;
+    vertical-align: sub;
+    padding-top: 6px;
+    display: inline-block;
+    font-size: 15px;
+    color: ${txtColor};
+  }
+  input {
+    display: none;
+    visibility: hidden;
+    &:checked ~ .custom-radio:before {
+      background-color: ${beforeColor};
+    }
+    &:checked ~ .custom-radio {
+      border-color: ${checkedBorderColor};
+    }
+    &:checked ~ span {
+      color: ${checkedBorderColor};
+    }
+  }
+  .custom-radio {
+    border: 3px solid ${txtColor};
+    width: 9px;
+    height: 9px;
+    ${'' /* border-radius: 50%; */}
+    float: left;
+    position: relative;
+    transition: border 0.3s linear;
+    margin-top: 2px;
+
+    &:after {
+      content: '';
+      display: table;
+      clear: both;
+    }
+    &:before {
+      display: block;
+      position: absolute;
+      content: '';
+      border-radius: 50%;
+      height: 5px;
+      width: 5px;
+      top: 2px;
+      left: 2px;
+      transition: background 0.25s linear;
+    }
+  }
+  span {
+    padding-left: 5px;
+    display: inline-block;
+  }
+`
 // buttons for filtering between charts
 const CryptoLineFilter = ({ children, onChange, checked }) => {
   // console.log(checked)
   return (
-    <>
-      <label>{children}</label>
-      <input onChange={onChange} checked={checked} type="checkbox" />
-    </>
+    <div css={filterBoxes}>
+      <label>
+        <input onChange={onChange} checked={checked} type="checkbox" />
+        <span>{children}</span>
+        <div className="custom-radio" />
+      </label>
+    </div>
   )
 }
 
 export default CryptoLineFilter
+
+CryptoLineFilter.propTypes = {
+  children: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  checked: PropTypes.bool.isRequired
+}
