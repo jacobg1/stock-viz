d66bb80ccdf4142d6a802d3b4062b645c8b12c0d
Jacob Greenwald <jeg4t@virginia.edu>
2019-03-15 19:16:23 -0400

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import ChartsContainer from '../src/components/Charts'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(thunk))
+)
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+	const div = document.createElement('div')
+	ReactDOM.render(
+		<Provider store={store}>
+			<ChartsContainer />
+		</Provider>,
+		div
+	)
+	ReactDOM.unmountComponentAtNode(div)
 })
